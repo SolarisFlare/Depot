001fe3d0dd29d85a2b6b4afd8590cefe84f6323a
SolarisFlare <shadowbomb20@gmail.com>
2019-06-03 11:46:11 -1000

    Resource free scaffold complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 Product.delete_all
 
 Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
-               description:
-                %{<p>
-                <em>Native Apps, Multiple Platforms</em>
-                Answer the questions "Can we build this for ALL the devices?" with a resounding YEs.  This book will help you get there with a real-world introduction to seven platforms, whether you'rs new to mobile or an experienced developer needing to exapnd your options.  Plus you'll find out which cross-platform solution makes the most sense for your needs.</p>},
-                image_url: '7apps.jpg',
+                description: %{<p><em>Native Apps, Multiple Platforms</em>Answer the questions "Can we build this for ALL the devices?" with a resounding YEs.  This book will help you get there with a real-world introduction to seven platforms, whether you'rs new to mobile or an experienced developer needing to exapnd your options.  Plus you'll find out which cross-platform solution makes the most sense for your needs.</p>},
+                image_url: '',
                 price: 26.00)
+
