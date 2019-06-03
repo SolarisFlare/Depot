0deee6b8a268f5ea80c488c40128f1e8d4a3eaef
SolarisFlare <shadowbomb20@gmail.com>
2019-06-03 13:44:27 -1000

    Fixed application and added tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_203523) do
+ActiveRecord::Schema.define(version: 2019_06_03_225305) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "image_url"
-    t.decimal "price", precision: 8, scale: 2
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
