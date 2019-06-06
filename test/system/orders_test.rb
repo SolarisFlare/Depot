d20f27b50f02ca465bbfa45df267bad0cb6e9cae
SolarisFlare <shadowbomb20@gmail.com>
2019-06-06 07:37:12 -1000

    Added slow payment processor queued jobs

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,67 +1,64 @@
+#---
+# Excerpted from "Agile Web Development with Rails 5.1",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails51 for more book information.
+#---
 require "application_system_test_case"
 
+
 class OrdersTest < ApplicationSystemTestCase
-  setup do
-    @order = orders(:one)
-  end
-
-  test "visiting the index" do
-    visit orders_url
-    assert_selector "h1", text: "Orders"
-  end
-
-  test "creating a Order" do
-    visit orders_url
-    click_on "New Order"
-
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Create Order"
-
-    assert_text "Order was successfully created"
-    click_on "Back"
-  end
-
-  test "updating a Order" do
-    visit orders_url
-    click_on "Edit", match: :first
-
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Update Order"
-
-    assert_text "Order was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Order" do
-    visit orders_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Order was successfully destroyed"
-  end
+  include ActiveJob::TestHelper
+    
 
   test "check routing number" do
-      visit store_index_url
 
-      first('.catalog li').click_on 'Add to Cart'
+    LineItem.delete_all
+    Order.delete_all
 
-      click_on 'Checkout'
+    visit store_index_url
 
-      fill_in 'order_name', with: 'Dave Thomas'
-      fill_in 'order_address', with: '123 Main Street'
-      fill_in 'order_email', with: 'dave@example.com'
+    visit store_index_url
 
-      assert_no_selector "#order_routing_number"
+    first('.catalog li').click_on 'Add to Cart'
 
-      select 'Check', from: 'pay_type'
+    click_on 'Checkout'
 
-      assert_selector "#order_routing_number"
-  end
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    assert_no_selector "#order_routing_number"
+
+    select 'Check', from: 'pay_type'
+
+
+    assert_selector "#order_routing_number"
+
+    fill_in "Routing #", with: "123456"
+    fill_in "Account #", with: "987654"
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Dave Thomas",      order.name
+    assert_equal "123 Main Street",  order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check",            order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"],                 mail.to
+    assert_equal 'Sam Ruby <depot@example.com>',       mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+
+  end 
 end
